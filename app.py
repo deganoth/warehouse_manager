244a4c753df5697c7bf110b2858ff47b09aecb74
deganoth <oliver.deegan@gmail.com>
2020-01-03 18:13:52 +0000

    connected to MongoDB, created test html documents

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'shop_inventory'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Ornagy13@myfirstcluster-vsdxp.mongodb.net/shop_inventory?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello_world():
-    return 'Hello World ...again'
+@app.route('/get_products')
+def get_products():
+    return render_template('products.html', products=mongo.db.products.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
