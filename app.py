efaec46818e1862f9b6c6eadc0d709f8ef3ae9f8
deganoth <oliver.deegan@gmail.com>
2020-01-06 18:18:42 +0000

    edit product page created with some functionality added for editing a product. ongoing

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ mongo = PyMongo(app)
 
 @app.route('/')
 
+# Display the dashboard.html as the primary webpage and retrieve the relevant data from mongoDB
 @app.route('/get_dashboard')
 def get_dashboard():
     return render_template('dashboard.html', 
@@ -20,12 +21,14 @@ def get_dashboard():
     	manufacturers=mongo.db.products.find()
     	)
 
+# Display the products.html webpage and retrieve the relevant data form mongoDB for display
 @app.route('/get_products')
 def get_products():
 	return render_template('products.html', 
 		products=mongo.db.products.find(), 
     	)
 
+# Make use of the data required from mongo to create dropdown menues for use in creating a new product
 @app.route('/add_product')
 def add_product():
 	return render_template('addproduct.html', 
@@ -34,11 +37,25 @@ def add_product():
 		suppliers=mongo.db.suppliers.find()
     	)
 
+# Convert the information collected on the addproduct.html webpage and send it back to mongoDB for insertion
 @app.route('/insert_product', methods=['POST'])
 def insert_product():
 	products = mongo.db.products
 	products.insert_one(request.form.to_dict())
 	return redirect(url_for('get_products'))
+
+# determine between different products by calling on the object id stored with each product. 
+@app.route('/edit_product/<product_id>')
+def edit_product(product_id):
+	the_product = mongo.db.products.find_one({'_id': ObjectId(product_id)})
+	all_manufacturers = mongo.db.manufacturers.find()
+	all_categories = mongo.db.categories.find()
+	all_suppliers = mongo.db.suppliers.find()
+	return render_template('editproduct.html', 
+		product=the_product,
+		manufacturers=all_manufacturers, 
+		categories=all_categories,
+		suppliers=all_suppliers)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
