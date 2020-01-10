ff8fd2ec276cc7630024b5ce713bf055779f0629
deganoth <oliver.deegan@gmail.com>
2020-01-10 00:21:33 +0000

    search function started

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from flask import Flask, render_template, redirect, request, url_for, json
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -15,25 +15,39 @@ mongo = PyMongo(app)
 # Display the dashboard.html as the primary webpage and retrieve the relevant data from mongoDB
 @app.route('/get_dashboard')
 def get_dashboard():
-    return render_template('dashboard.html', 
-    	bar_name="Products",
-    	bar_quantity=mongo.db.products.find(),
-    	bar_product=mongo.db.products.find(),
-    	pie_name="Categories",
-    	pie_quantity=mongo.db.products.find(),
-    	pie_category=mongo.db.products.find(),
-    	products=mongo.db.products.find(), 
-        categories=mongo.db.products.find(), 
-    	manufacturers=mongo.db.products.find()
-    	)
+	return render_template('dashboard.html', 
+		bar_name="Products",
+		bar_quantity=mongo.db.products.find(),
+		bar_product=mongo.db.products.find(),
+		pie_name="Categories",
+		pie_quantity=mongo.db.products.find(),
+		pie_category=mongo.db.products.find(),
+		products=mongo.db.products.find(), 
+		categories=mongo.db.products.find(), 
+		manufacturers=mongo.db.products.find()
+		)
 
 # Display the products.html webpage and retrieve the relevant data form mongoDB for display
 @app.route('/get_products')
 def get_products():
 	return render_template('products.html', 
 		products=mongo.db.products.find()
-    	)
-	
+		)
+
+# search the products collection for matching products based on the product name description, manufacturer
+@app.route('/search_products', methods=['POST'])
+def search_products():
+	search_text_input = request.args['search_text']
+	search_text = {'$regex': re.compile('.*{}.*'.format(search_text_input)), '$options':'i'}
+	results = mongo.db.products.find({
+		'$or': [
+		{'product_name': query},
+		{'product_description': query},
+		{'manufacturer_name': query},
+		]
+		})
+	return render_template('searchresults.html', search_text=search_text_input, results=results)
+
 # Make use of the data required from mongo to create dropdown menues for use in creating a new product
 @app.route('/add_product')
 def add_product():
@@ -41,7 +55,7 @@ def add_product():
 		manufacturers=mongo.db.manufacturers.find(), 
 		categories=mongo.db.categories.find(),
 		suppliers=mongo.db.suppliers.find()
-    	)
+		)
 
 # Convert the information collected on the addproduct.html webpage and send it back to mongoDB for insertion
 @app.route('/insert_product', methods=['POST'])
@@ -61,34 +75,37 @@ def edit_product(product_id):
 		product=the_product,
 		manufacturers=all_manufacturers, 
 		categories=all_categories,
-		suppliers=all_suppliers)
+		suppliers=all_suppliers
+		)
 
 @app.route('/update_product/<product_id>', methods=["POST"])
 def update_product(product_id):
-    products = mongo.db.products
-    products.update( {'_id': ObjectId(product_id)},
-    {
-        'product_name':request.form.get('product_name'),
-        'product_description':request.form.get('product_description'),
-        'manufacturer_name': request.form.get('manufacturer_name'),
-        'supplier_name': request.form.get('supplier_name'),
-        'category_name':request.form.get('category_name'),
-        'product_price':request.form.get('product_price'),
-        'product_quantity':request.form.get('product_quantity'),
-        'product_status':request.form.get('product_status'),
-        'product_EAN':request.form.get('product_EAN')
-    })
-    return redirect(url_for('get_products'))
+	products = mongo.db.products
+	products.update( {'_id': ObjectId(product_id)},
+		{
+		'product_name':request.form.get('product_name'),
+		'product_description':request.form.get('product_description'),
+		'manufacturer_name': request.form.get('manufacturer_name'),
+		'supplier_name': request.form.get('supplier_name'),
+		'category_name':request.form.get('category_name'),
+		'product_price':request.form.get('product_price'),
+		'product_quantity':request.form.get('product_quantity'),
+		'product_status':request.form.get('product_status'),
+		'product_EAN':request.form.get('product_EAN')
+		})
+	return redirect(url_for('get_products'))
 
 @app.route('/delete_product/<product_id>')
 def delete_product(product_id):
 	mongo.db.products.remove({'_id': ObjectId(product_id)})
-	return redirect(url_for('get_products'))
+	return redirect(url_for('get_products')
+		)
 
 @app.route('/get_categories')
 def get_categories():
 	return render_template('categories.html',
-		categories=mongo.db.categories.find())
+		categories=mongo.db.categories.find()
+		)
 
 @app.route('/add_category')
 def add_category():
@@ -97,6 +114,6 @@ def add_category():
 		)
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), 
-    	port=os.environ.get('PORT'), 
-    	debug=True)
+	app.run(host=os.environ.get('IP'), 
+		port=os.environ.get('PORT'), 
+		debug=True)
