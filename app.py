20ee9edf508d447e84bae0c13da51852eeb10edb
deganoth <oliver.deegan@gmail.com>
2020-01-20 22:30:40 +0000

    tested environment variable for github deployment

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'shop_inventory'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+app.config["MONGO_URI"] = 'mongodb+srv://root:Ornagy13@myfirstcluster-vsdxp.mongodb.net/shop_inventory?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
