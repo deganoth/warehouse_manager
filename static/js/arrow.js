2801244c1c0d36b9b4cc4db13e2e085957509d06
deganoth <oliver.deegan@gmail.com>
2020-01-30 21:51:03 +0000

    javascript functions commented

--- static/js/arrow.js
+++ static/js/arrow.js
@@ -1,3 +1,8 @@
+/* this function targets a class assocaited with the product table header elements. It toggles a class designed to 
+create a small triangle to simulate sorting through the sort function. The class swap changes the orientation
+from facing down, to up, depending on whcih sort function is in play.
+ */
+
 function arrow(n) {
   var element = document.getElementById("productsTable");
   element.getElementsByTagName("TH")[n].classList.toggle("headerSortUp");
--- static/js/materialize_initialize_script.js
+++ static/js/materialize_initialize_script.js
@@ -1,8 +1,10 @@
+/* part of the materialize requirements, each javasript elemtn to be use within the classes must be called in a document ready function */
+
 $(document).ready(function() {
-            $('.sidenav').sidenav();
-            $('.collapsible').collapsible();
-            $('.dropdown-trigger').dropdown();
-            $('select').formSelect();
-            $('.modal').modal();
-        });
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.dropdown-trigger').dropdown();
+    $('select').formSelect();
+    $('.modal').modal();
+});
 
